2659b8f4a772582ec9981572d8c48d9a1a2afdca
Iulia Florea <iulia.florea@enea.com>
2020-03-06 16:28:11 +0100

    Add labels for Gene Bilogic Process example

---
+++ Predefined Labels - Biologic Process.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+cellular_process_info = ['cell', 'adhesion', 'communication', 'signal_transduction', 'extracellular_stimulus', 'hormone_secretion',
+                         'intracellular_signaling_cascade', 'enzyme_linked', 'receptor_protein', 'small_GTPase-mediated_signal_transduction',
+                         'transmembrane', 'signaling_pathway', 'protein_kinase', 'notch_pathway', 'NF-kB', 'VEGF', 'regulation', 'activity']
+
+development_info = ['morphogenesis', 'organ_development', 'tissue', 'cell_differentiation', 'epithelial', 'epidermal', 'epidermis', 'keratinization',
+                    'mesenchymal_differentiation_transition','ectoderm', 'mesoderm', 'anglogenesis', 'vasculature', 'blood_vessel', 'morphogenesis',
+                    'cellular', 'projection', 'organization', 'biogenesis', 'pseudopodium', 'formation', 'filopodium', 'microvilius_biogenesis',
+                    'epithelium', 'epihtelial']
+
+physiological_process_info = ['cellular_physiological_process', 'cell', 'motility', 'death', 'organization', 'biogenesis', 'cycle', 'cellular_morphogenesis',
+                              'cycle', 'substrate_junction_assembly', 'cytoskeleton_organization_biogenesis', 'actin_cytoskeleton', 'intermediate_filament',
+                              'organization_biogenesis', 'proliferation', 'regulation']
+
+domains = ['cellular_process', 'development', 'physiologic_process']
+
+themes = ['biologic_process']
+
+
+
+
+
+
